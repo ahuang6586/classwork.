c83e158870f16614ad25b6d8af5349c8d06f687f
Student 6 <Student 6@10.8.33.152>
2016-09-21 13:19:47 -0400

    g

--- AlexIntroduction/src/datatypes6/MathRandom.java
+++ AlexIntroduction/src/datatypes6/MathRandom.java
@@ -8,18 +8,39 @@ public class MathRandom {
 	}
 
 	public static void main(String[] args) {
-		for(int index = 0; index < 10; index++){
-			System.out.println("Roll #"+(index+1)+ " Die is"+rollFairDie());
+		int[] results = new int[6];
+		//same as 
+		// int[] results = {0,0,0,0,0,0};
+		int totalRolls = 10000;
+		for(int index = 0; index < totalRolls; index++){
+			int roll = rollUnfairDie();
+			System.out.println("Roll #"+(index+1)+ " Die is"+rollUnfairDie());
+			results[roll-1]++;
+		}
+		for(int i = 0; i < 6; i++){
+			int percentage = (int) (100*((double)results[i]/totalRolls));
+			System.out.println((i+1)+" was rolled "+ percentage+"%.");
 		}
 		
 	}
 	
-	public static int rollFairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6)+1;
-	}
+	//public static int rollFairDie(){
+	//	Random gen = new Random();
+	//	return gen.nextInt(6)+1;
+		//double rand = Math.random();//creates random double (0,1) rand can only numbers between 0 and 1
+	//	int roll = (int) (6 * rand);//[0,5] 
+		//return roll + 1; // 0 becomes 1, 5 becomes 6
+	//}
 	public static int rollUnfairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6)+1;
+		double rand = Math.random();
+		int roll = (int) (6 * rand);
+		if (roll == 6)
+		return roll - 1;
+		
+		
+		return roll;
+		
+		
+		
 	}
 }
