c19e29acce9efe8db1bd9e3117fccc41e98b2fc2
ahuang6586 <ahuang6586@bths.edu>
2016-12-22 23:28:58 -0500

    Update MyScreen.java

--- AlexIntroduction/src/guis/components/MyScreen.java
+++ AlexIntroduction/src/guis/components/MyScreen.java
@@ -1,14 +1,26 @@
 package guis.components;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+
+import guiPractice.Screen;
+import guiPractice.components.Action;
+import guiPractice.components.Button;
+import guiPractice.components.ClickableGraphic;
+import guiPractice.components.Graphic;
+import guiPractice.components.Visible;
 
 import GUI.Screen;
 
-public class MyScreen extends Screen{
+public class MyScreen extends Screen implements MouseMotionListener, MouseListener{
 
 	
 	private Visible face;
+	private Graphic picture;
+	
 	public MyScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -17,7 +29,60 @@ public class MyScreen extends Screen{
 		
 		face = new Graphic(30,40,100,100,"resources/sampleImages/face.jpg");
 		viewObjects.add(face);
+		picture=new ClickableGraphic(0,0,0.2,"resourses/sampleImages/face.png");
+		viewObjects.add(pictureTwo);
+		
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(back.isHovered(e.getX(), e.getY())){
+			back.act();
+			}
 	}
 	
+	public MouseMotionListener getMouseMotionListener(){
+		return this;
+	}
+	public MouseListener getMouseListener(){
+		return this;
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
 	
 }
