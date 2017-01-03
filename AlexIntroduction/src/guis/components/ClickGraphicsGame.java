57e481dcef2a8ea56dc5099a3c809644944a5642
user <user@192.168.1.10>
2017-01-02 23:47:50 -0500

    stuff

--- AlexIntroduction/src/guis/components/ClickGraphicsGame.java
+++ AlexIntroduction/src/guis/components/ClickGraphicsGame.java
@@ -1,9 +1,89 @@
 package guis.components;
 
-public class ClickGraphicsGame {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
-	public ClickGraphicsGame() {
+import GUI.Screen;
+import guis.components.Action;
+import guis.components.ClickableGraphic;
+import guis.components.Visible;
+
+public class ClickGraphicsGame extends Screen implements MouseMotionListener,MouseListener{
+	
+	private ClickableGraphic pic;
+	public ClickGraphicsGame(int width,int height) {
 		super(500,500);
 	}
+
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(pic.isHovered(e.getX(), e.getY())){
+			pic.act();
+		}
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		pic = new ClickableGraphic(500,30,200,200,"resources/sampleImages/face.jpg");
+		pic.setAction(new Action(){
+			public void act(){
+				pic.setX(pic.getX()+10);
+			}
+		});
+		viewObjects.add(pic);
+
+	}
+	public MouseListener getMouseListener(){
+		return this;
+	}
+
+	public MouseMotionListener getMouseMotionListener(){
+		return this;
+	}
+
+	
+	
+
 
 }
--- AlexIntroduction/src/guis/components/ClickableScreen.java
+++ AlexIntroduction/src/guis/components/ClickableScreen.java
@@ -2,35 +2,29 @@ package guis.components;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import GUI.Screen;
 
-public class ClickableScreen extends Screen implements MouseListener{
+public abstract class ClickableScreen extends Screen implements MouseMotionListener,MouseListener {
 
-	private Visible clickables;
-	public abstract void initAllObjects(ArrayList<Visible> );
 	public ClickableScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public void initObjects(ArrayList<Visible> viewObjects) {
-		initAllObjects(viewObjects);
-		clickables = new ArrayList<Clickable>();
-		for(Visbile v: viewObjects){
-			if(v instanceof Clickable){
-				clickables.add((Clickable)v);
-			}
-		}
-		
-	}
-
-	@Override
+	protected ArrayList<Clickable> clickables;
+	
+		// TODO Auto-generated constructor stub
+	
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		for(Clickable object: clickables){
+			if(object.isHovered(getWidth(), getHeight())){
+				object.act();
+				break;
+			}
+		}
 	}
 
 	@Override
@@ -59,8 +53,50 @@ public class ClickableScreen extends Screen implements MouseListener{
 	 public MouseListener getMouseListener(){
 		 return this;
 	 }
+	 public MouseMotionListener getMouseMotionListener(){
+		 return this;
+	 }
+	 
+	 public void addObject(Visible v){
+		 super.addObject(v);
+		 if(v instanceof Clickable){
+		 clickables.add((Clickable)v);
+		 }
+		 }
+		 
+
+		 
+		 public void remove(Visible v){
+		 super.remove(v);
+		 clickables.remove((Clickable)v);
+		 } 
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		initAllObjects(viewObjects);
+		clickables=new ArrayList<Clickable>();
+		for(Visible object: viewObjects){
+			if(object instanceof Clickable){
+				clickables.add((Clickable)object);
+			}
+		}
+		
+		
+	
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	 
 		 
-	 
+	public abstract void initAllObjects(ArrayList<Visible> viewObjects);
 
 }
